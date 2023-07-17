d4e2512377f34c0cf9d40d27154fb256cd899ade
student <student@gnu.ac.in>
2023-07-17 08:25:23 +0530

    kotlin

---
+++ src/main/kotlin/pratical1_1.1.kt
@@ -0,0 +1,19 @@
+fun main (){
+    val a : Int =10
+    val b : Float= 4.2f
+    val c : Char  = 'j'
+    val d : String = "jay"
+    val e : Double = 12.78
+    val f : Long = 2314302
+    val g : Short = -1
+    val h : Byte = 1
+
+    println(" $a is inter value ")
+    println(" $b  is float  value ")
+    println(" $c  is char value ")
+    println(" $d is string value ")
+    println(" $e is double value ")
+    println(" $f is i long value ")
+    println(" $g is short value ")
+    println(" $h is i byte value ")
+}
---
+++ src/main/kotlin/praticcal1 _1.2.kt
@@ -0,0 +1,6 @@
+fun main () {
+    val a: Int = 10
+    val b = a.toDouble()
+
+    println("$b to double value")
+}
